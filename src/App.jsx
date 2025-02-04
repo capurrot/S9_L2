828d6791ba883c4f397c235c94e03a807fdcf208
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-02-04 13:16:12 +0000

    creazione cartella

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import MyFooter from "./components/MyFooter";
+import MyNav from "./components/MyNav";
+
+function App() {
+  return (
+    <>
+      <MyNav />
+      <MyFooter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,9 @@
+const MyFooter = () => {
+  return (
+    <footer className="bg-dark text-white py-4 text-center">
+      <p className="text-sm">&copy; {new Date().getFullYear()} CiccioPasticcio. Tutti i diritti riservati.</p>
+    </footer>
+  );
+};
+
+export default MyFooter;
